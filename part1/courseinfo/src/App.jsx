85460dc126d22aac23cec98d0523a45ab8e9db3d
Sush <sushrutpatil5388@outlook.com>
2025-05-26 14:26:08 +0530

    Small changes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,57 +1,18 @@
-import React from "react";
-
-const Header = (props) => {
+const Hello = () => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <p>Hello world</p>
     </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((part, index) => (
-        <Part key={index} part={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <div>
-      <p>Number of exercises {total}</p>
-    </div>
-  );
-};
+  )
+}
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
-
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  );
-};
+      <h1>Greetings</h1>
 
-export default App;
+      <Hello />
+    </div>
+  )
+}
+export default App
