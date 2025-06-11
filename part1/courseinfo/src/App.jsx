0364820f457fda2580e2ac7d234ff8b531b959be
Sush <sushrutpatil5388@outlook.com>
2025-06-11 23:58:26 +0530

    exercise 1.2 completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,57 +1,58 @@
-const App=()=>{
-  const course='Half Stack application development'
-  const part1='Fundamentals of React'
-  const exercises1=10
-  const part2='Using props to pass data'
-  const exercises2=7
-  const part3='State of a component'
-  const exercises3=14
+import React from "react";
 
-
-  return(
-    <div>
-      <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-
-      <Total total={exercises1+exercises2+exercises3}/>
-
-     
-
-    </div>
-  )
-}
-
-const Header=(props)=>{
-  return(
+const Header = (props) => {
+  return (
     <div>
       <h1>{props.course}</h1>
     </div>
-  )
+  );
+};
 
-}
-
-const Content=(props)=>{
-  return(
-    <div>
-      <p>{props.part}{props.exercises}</p>
-    </div>
-  )
-
-}
-
-const Total=(props)=>{
-  return(
+const Part = (props) => {
+  return (
     <div>
       <p>
-        No of exercises{props.total}
+        {props.part} {props.exercises}
       </p>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
 
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  );
+};
 
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
-export default App
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
+
